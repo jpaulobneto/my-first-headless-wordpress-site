906990294778568e45c59278fd21a697fd049d2e
João Paulo Barbosa <joao.barbosa@rdstation.com>
2022-06-03 12:29:51 -0300

    feat: implement blog index page

--- pages/blog.js
+++ pages/blog.js
@@ -1,11 +1,26 @@
 import { gql } from "@apollo/client";
-
-import { client } from "../lib/apolloClient";
 import Layout from "../components/Layout";
 import PostsList from "../components/PostsList";
+import { client } from "../lib/apolloClient";
 
-// Dummy data
-import { posts } from "../dummy-data";
+const GET_POSTS = gql`
+  query getPosts {
+    posts(first: 18, after: null) {
+      nodes {
+        databaseId
+        title
+        excerpt
+        uri
+        featuredImage {
+          node {
+            sourceUrl
+            altText
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default function Blog(props) {
   const { posts } = props;
@@ -19,9 +34,13 @@ export default function Blog(props) {
 }
 
 export async function getStaticProps() {
+  const response = await client.query({
+    query: GET_POSTS,
+  });
+
   return {
     props: {
-      posts: posts,
+      posts: response.data.posts.nodes,
     },
   };
 }
